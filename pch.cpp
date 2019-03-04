4c02208077b72dbe8436eb1a6b9bf7a8e660d1b9
dostoevsky-plus <dostoevsky-plus@yandex.ru>
2019-03-04 11:55:06 +0700

    1 лаба
    залил 1 лабу

---
+++ pch.cpp
@@ -0,0 +1,121 @@
+﻿// pch.cpp: исходный файл, соответствующий предкомпилированному заголовку; нужен для компиляции
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+	void plane::InData(ifstream &ifst) {
+		ifst >> c  >> range ;
+	}
+	void traine::InData(ifstream &ifst) {
+		ifst >> count ;
+	}
+	void plane::Out(ofstream &ofst) {
+		ofst << "It is Plane: грузоподъемность = " << c
+			<< ", дальность полета = " << range;
+		OutCommon(ofst);
+
+	}
+	void traine::Out(ofstream &ofst) {
+		ofst << "It is Traine: count = " << count ;
+		OutCommon(ofst);
+	}
+	transport* transport::In(ifstream &ifst)
+	{
+		transport *s;
+		int key;
+		ifst >> key;
+		if (key == 1) {
+			s = new plane;
+		}
+		else if (key == 2)
+		{
+			s = new traine;
+		}
+		else
+		return 0;
+		s->InData(ifst);
+		ifst >> s->distance >> s->spead;
+		return s;
+	}
+	void transport::OutCommon(ofstream & ofst)
+	{
+		ofst << ", расстояние между пунктами = " << distance << ", скорость = " << spead << endl;
+	}
+	container::Node::Node()
+	{
+		this->Next = nullptr;
+		this->Prev = nullptr;
+		this->data = nullptr;
+	}
+	
+	container::container()
+	{
+		Top = nullptr;
+		count = 0;
+	}
+	void container::Clear()
+	{
+		Node* current = Top;
+		for (int i = 1; i < count; i++)
+		{
+			current = current->Next;
+			delete current->Prev;
+		}
+		delete current;
+		count = 0;
+	}
+	void container::In(ifstream & ifst)
+	{
+		int flag;
+		while (!ifst.eof()) {
+			if (count == 0)
+			{
+				Top = new Node;
+				Top->Next = Top;
+				Top->Prev = Top;
+				if ((Top->data= transport::In(ifst)) != 0)
+					flag = 1;
+				else
+					flag = 0;
+				
+			}
+			else
+			{
+				Node *current = Top;
+				for (int j = 1; j < count; j++)
+				{
+					current = current->Next;
+				}
+				current->Next = new Node;
+				if ((current->Next->data = transport::In(ifst)) != 0)
+				{
+					Top->Prev = current->Next;
+					current->Next->Prev = current;
+					current->Next->Next = Top;
+					flag = 1;
+				}
+				else
+				{
+					flag = 0;
+				}
+			}
+			if (flag  != 0)
+				count++;
+		}
+	}
+
+	void container::Out(ofstream & ofst)
+	{
+		Node* current = Top;
+		ofst << " Container contains " << count
+			<< " elements." << endl;
+		for (int j = 0; j < count; j++) {
+			ofst << j << ": ";
+			current->data->Out(ofst);
+			current = current->Next;
+		}
+	}
+
+// В целом этот файл можно пропустить, но не удаляйте его, если вы используете предкомпилированные заголовки.
+	
---
+++ pch.h
@@ -0,0 +1,65 @@
+﻿// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
+#ifndef PCH_H
+#define PCH_H
+#include <fstream>
+using namespace std;
+class transport {
+private:
+	int spead;
+	int distance;
+public:
+	static transport* In(ifstream &ifst);
+	virtual void InData(ifstream &ifst) = 0; // ввод
+	virtual void Out(ofstream &ofst) = 0; // вывод
+	void OutCommon(ofstream &ofst);
+};
+class container
+{
+private:
+	class Node {		
+	public:
+		Node();
+		Node* Next;
+		Node* Prev;
+		transport* data;
+	};
+	Node* Top;
+	int count;
+public:
+	void In(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	void Clear(); // очистка контейнера от фигур
+	container(); // инициализация контейнера
+	~container() { Clear(); }
+};
+
+
+class plane : public transport {
+	int range;
+	int c;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	plane() {} // создание без инициализации.
+};
+class traine : public transport {
+	int count;
+
+public:
+	void InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst); // вывод
+	traine() {} // создание без инициализации.
+};
+
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H
